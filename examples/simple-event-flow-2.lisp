65cdcc5b4ad0edfeaec92e837c3c967eaea1ed1b
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-05-23 23:35:05 +0200

    Improve the dataflow example a bit.

--- examples/simple-event-flow-2.lisp
+++ examples/simple-event-flow-2.lisp
@@ -3,9 +3,9 @@
 (in-package #:sw)
 
 
-(defclass simple-event-flow-2 (application self-ref)
-  ((click-me-widget :initform ¤(html-element :model #~"Click me!"))
-   (x :initform 0)
+;; Model?
+(defclass simple-event-flow-2 (application)
+  ((x :initform 0)
    (square-of-x :initform ↑λ(* ¤x ¤x)))
 
   (:metaclass mvc-stm-class))
@@ -13,17 +13,32 @@
 (set-uri 'simple-event-flow-2 "/simple-event-flow-2")
 
 
-(defmethod main ((app simple-event-flow-2))
+;; Controller?
+(defmethod incf-x ((app simple-event-flow-2))
   (with-object app
-    
-    λ(when (mouse-click-state-of ¤click-me-widget)
-       (incf ¤x))))
+    (incf ¤x)))
 
 
+;; View?
 (defmethod render-viewport ((viewport viewport) (app simple-event-flow-2))
   (with-object app
-    (add-to (root)
-            ¤click-me-widget
-            ¤(html-element :model #λ(format nil "X: ~A" ¤x))
-            ¤(html-element :model #λ(format nil "SQUARE-OF-X: ~A" ¤square-of-x))
-            ¤(html-element :model #λ(format nil "X + X: ~A" (+ ¤x ¤x))))))
+    (let ((click-me-widget ¤(html-element :model #~"Click me!")))
+      
+      λ(when (mouse-click-state-of click-me-widget)
+         (incf-x app)) ;; ..calls Controller.
+      
+      (add-to (root)
+              click-me-widget
+              ¤(html-element :model #λ(fmtn "X: ~A" ¤x))
+              ¤(html-element :model #λ(fmtn "SQUARE-OF-X: ~A" ¤square-of-x))))))
+
+
+#|
+Is this MVC "done right"? I can manipulate the Model via the Controller from the REPL:
+
+  > (incf-x *app*)
+
+
+|#
+
+
