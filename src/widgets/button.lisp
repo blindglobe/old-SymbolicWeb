fea26b50dcea330224504437887b7d533a7347f0
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-06-08 20:18:14 +0200

    MK-BUTTON added and exported.
    SW> (mk-button "Hello World")
    #<BUTTON-BA :MODEL #<DLIST {D0BF419}>>
    SW> (mk-button #~"Hello World")
    #<BUTTON-BC :MODEL #<DLIST {D17D8F1}>>
    SW> (mk-button (dlist #~"a" #~"b" #~"c"))
    #<BUTTON-BE :MODEL #<DLIST {D2F8811}>>
    SW>

--- src/widgets/button.lisp
+++ src/widgets/button.lisp
@@ -10,3 +10,20 @@
 
   (:default-initargs
    :element-type "button"))
+(export 'button)
+
+
+(defun mk-button (content)
+  (let* ((container-view (make-instance 'button))
+         (container-model ~container-view))
+    (prog1 container-view
+      (typecase content
+        (string
+         (sw-mvc:insert #~content :in container-model))
+
+        ((or single-value-model sw-mvc:container)
+         (sw-mvc:insert content :in container-model))
+
+        (otherwise
+         (error "Don't know what to do with ~S." content))))))
+(export 'mk-button)
