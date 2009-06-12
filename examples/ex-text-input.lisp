03ad3bccfa7ba332671edf1219d67386588b19d5
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-06-13 01:38:20 +0200

    Uninteresting fixes.

--- examples/ex-text-input.lisp
+++ examples/ex-text-input.lisp
@@ -5,16 +5,20 @@
 
 (defclass text-input-app (application)
   ((text-input :initform (mk-text-input () "Hello"))
-   (value-observer :initform ↑λ(format t "value: ~S~%" ~~¤text-input))
+   (value-observer :initform ↑λ(format t "value changed to: ~S~%" ~~¤text-input))
    (enter-observer :initform ↑λ(when (enterpress-state-of ¤text-input)
-                                 (write-line "enter pressed!"))))
+                                 (format t "enter pressed, and value is: ~S~%" ~~¤text-input)))
+
+   (integer-input :initform (mk-text-input () 42)))
 
   (:metaclass mvc-stm-class))
 
 (set-uri 'text-input-app "/text-input")
 
 
-
 (defmethod render-viewport ((viewport viewport) (app text-input-app))
   (with-object app
-    (add-to (root) ¤text-input)))
+    (add-to (root)
+     (mk-htmlc :div
+       (:h1 "TEXT-INPUT-APP")
+       (:sw ¤text-input)))))
