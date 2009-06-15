ff1df3f540e63dc1451b66e4554ed6823490cad7
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-06-15 08:14:06 +0200

    Done some work on input validation + feedback wrt. the TEXT-INPUT example.
    This is very verbose and needs more work.

--- examples/ex-text-input.lisp
+++ examples/ex-text-input.lisp
@@ -7,7 +7,9 @@
   ((x :initform #~5)
    (y :initform #~17)
    (square-of-x :initform ↑#λ(* ¤x ¤x))
-   (sum :initform ↑#λ(+ ¤square-of-x ¤y)))
+   (sum :initform ↑#λ(+ ¤square-of-x ¤y))
+
+   (x-view :initform ↑(mk-text-input (:model (cell-of ¤x)))))
 
   (:metaclass mvc-stm-class))
 
@@ -18,8 +20,17 @@
   (with-object app
     (setf (input-translator-of (cell-of ¤x))
           (lambda (input)
-            (handler-case (number-input-translator input)
-              (error () ¤x)))
+            (handler-case
+                (unwind-protect-case ()
+                    (number-input-translator input)
+                  (:abort
+                   (unless (eq t (feedback-event-of (cell-of ¤x)))
+                     (tf (feedback-event-of (cell-of ¤x)))))
+                  (:normal
+                   (unless (eq nil (feedback-event-of (cell-of ¤x)))
+                     (nilf (feedback-event-of (cell-of ¤x))))))
+              (error ()
+                ¤x)))
 
           (input-translator-of (cell-of ¤y))
           (lambda (input)
@@ -29,17 +40,21 @@
 
 (defmethod render-viewport ((viewport viewport) (app text-input-app))
   (with-object app
-    (let ((x-view (mk-text-input (:model (cell-of ¤x)))))
-      (add-to (root)
-        (mk-html
-          (:div
-           (:h1 "TEXT-INPUT-APP")
-           "X: " (:sw x-view) :br
-           "SQUARE-OF-X: " (:sw (cell-of ¤square-of-x)) :br
-           "Y: " (:sw (mk-text-input (:model (cell-of ¤y)))) :br
-           "(+ SQUARE-OF-X Y): " (:sw (cell-of ¤sum))
-           :p
-           :hr
-           (:a :href "http://gitorious.org/symbolicweb/symbolicweb/blobs/master/examples/ex-text-input.lisp"
-               "source code"))))
-      (focus x-view))))
+    (add-to (root)
+      (mk-html
+        (:div
+         (:h1 "TEXT-INPUT-APP")
+         "X: " (:sw ¤x-view) (:sw (letp1 ((div (mk-div "need more cowbell" :display "none")))
+                                    (setf (formula-of div)
+                                          #λ(if (feedback-event-of (cell-of ¤x))
+                                                (setf (display-of div) "inline")
+                                                (setf (display-of div) "none")))))
+         :br
+         "SQUARE-OF-X: " (:sw (cell-of ¤square-of-x)) :br
+         "Y: " (:sw (mk-text-input (:model (cell-of ¤y)))) :br
+         "(+ SQUARE-OF-X Y): " (:sw (cell-of ¤sum))
+         :p
+         :hr
+         (:a :href "http://gitorious.org/symbolicweb/symbolicweb/blobs/master/examples/ex-text-input.lisp"
+             "source code"))))
+    (focus ¤x-view)))
