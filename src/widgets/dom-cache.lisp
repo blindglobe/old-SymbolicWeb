29f4c017aa03acda1167b85e0afc74d50b49b343
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-06-22 12:15:56 +0200

    Declarations and some muffling of compiler notes.

--- src/widgets/dom-cache.lisp
+++ src/widgets/dom-cache.lisp
@@ -68,13 +68,14 @@ I need to think about stuff like CSS-CLASS-OF.
                                 (dom-server-remover #'dom-server-remover)
                                 (value-marshaller #'princ-to-string)
                                 (value-removal-checker #'not)) ;; Essentially (lambda (value) (eq value nil)).
-  (declare (function dom-server-reader dom-server-writer dom-server-remover
-                     value-removal-checker))
+  (declare (function dom-server-reader dom-server-writer dom-server-remover))
+
 
   ;; Add DOM reader.
   (compile-and-execute
     `(defun ,lisp-reader-name (dom-mirror)
-       (declare (dom-mirror dom-mirror))
+       (declare #.(optimizations :dom-property)
+                (dom-mirror dom-mirror))
        (if *js-code-only-p*
            (funcall ,dom-client-reader ,dom-name dom-mirror)
            (multiple-value-bind (value found-p)
@@ -87,24 +88,29 @@ I need to think about stuff like CSS-CLASS-OF.
   ;; Add DOM writer.
   (compile-and-execute
     `(defun ,lisp-writer-name (property-value dom-mirror &rest args)
-       (declare (dom-mirror dom-mirror))
+       (declare #.(optimizations :dom-property)
+                (dom-mirror dom-mirror))
        (prog1 property-value
          (unless *js-code-only-p*
-           (if (funcall ,value-removal-checker property-value)
-               (progn
-                 (funcall ,dom-server-remover dom-mirror ',lisp-accessor-name)
-                 (apply ,dom-client-remover ,dom-name dom-mirror args))
-               (progn
-                 (funcall ,dom-server-writer dom-mirror ',lisp-accessor-name property-value)
-                 (apply ,dom-client-writer
-                        ,(if value-marshaller
-                             `(funcall ,value-marshaller property-value)
-                             `property-value)
-                        ,dom-name dom-mirror args)))))))
+           (muffle-compiler-note ;; For the (IF (AND ..)) check.
+             (if (and ,value-removal-checker (funcall ,value-removal-checker property-value))
+                 ;; Remove entry.
+                 (progn
+                   (funcall ,dom-server-remover dom-mirror ',lisp-accessor-name)
+                   (apply ,dom-client-remover ,dom-name dom-mirror args))
+                 ;; Add and/or set entry.
+                 (progn
+                   (funcall ,dom-server-writer dom-mirror ',lisp-accessor-name property-value)
+                   (apply ,dom-client-writer
+                          ,(if value-marshaller
+                               `(funcall ,value-marshaller property-value)
+                               `property-value)
+                          ,dom-name dom-mirror args))))))))
 
   ;; Add DOM renderer.
   (compile-and-execute
     `(defmethod render-dom ((dom-mirror dom-mirror) (property-name (eql ',lisp-accessor-name)) property-value)
+       (declare #.(optimizations :dom-property))
        (run (js-code-of (funcall ,dom-client-writer
                                  ,(if value-marshaller
                                       `(funcall ,value-marshaller property-value)
@@ -120,7 +126,7 @@ I need to think about stuff like CSS-CLASS-OF.
                                 (lisp-reader-name lisp-accessor-name)
                                 (lisp-writer-name (list 'setf lisp-accessor-name))
                                 (dom-server-remover #'dom-server-remover))
-
+  (declare (function dom-server-remover))
   ;; Remove entry from DOM-MIRROR-DATA hash-table and call DOM-CLIENT-REMOVER.
   ;; We do this by iterating through every active widget of *SERVER*.
   (with-each-viewport-in-server ()
