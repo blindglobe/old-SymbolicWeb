20d6840052dd6e20ca9246a6520bb63feacbae54
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-06-27 16:58:27 +0200

    Ensure that the order make somewhat sense.

--- src/widgets/html-container.lisp
+++ src/widgets/html-container.lisp
@@ -21,7 +21,7 @@
     (prog1 (call-next-method)
       (when-let ((additional-children (set-difference *html-container-children* (children-of html-container)
                                                       :test #'eq)))
-        (add-to* html-container additional-children))
+        (add-to* html-container (reverse additional-children)))
       (when-let ((removed-children (set-difference (children-of html-container) *html-container-children*
                                                    :test #'eq)))
         (dolist (child removed-children)
