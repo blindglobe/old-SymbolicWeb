a1b2a691129e05fcd97f6e6f693bac9b3cfa057e
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-07-12 15:24:13 +0200

    ...

--- examples/ex-text-input.lisp
+++ examples/ex-text-input.lisp
@@ -4,42 +4,31 @@
 
 
 (defclass text-input-app-model (self-ref)
-  ((x :initform #λ5)
-   (y :initform #λ17)
+  ((x :initform (with1 #λ5 (add-number-parser it t)))
+   (y :initform (with1 #λ17 (add-number-parser it t)))
    (square-of-x :initform ↑#λ(* ¤x ¤x))
    (sum :initform ↑#λ(+ ¤square-of-x ¤y)))
 
   (:metaclass mvc-class))
 
 
-#| X-VIEW and Y-VIEW are "session bound" (created "inside" the DEFCLASS form) to ensure that focus tracking
-works between page refreshing. |#
-
 
 (defclass text-input-app (text-input-app-model application)
   ((x-view :initform ↑(mk-text-input (:model (cell-of ¤x))))
-   (x-feedback :initform ↑(letp1 ((span (mk-elt :span "X needs more cowbell!")))
-                            (set-show-on-feedback span (cell-of ¤x))))
+   (x-feedback :initform ↑(with1 (mk-elt :span "X needs more cowbell!")
+                            (set-show-on-feedback it (cell-of ¤x))))
 
    (y-view :initform ↑(mk-text-input (:model (cell-of ¤y))))
-   (y-feedback :initform ↑(letp1 ((span (mk-elt :span "Y needs more cowbell!")))
-                            (set-show-on-feedback span (cell-of ¤y)))))
+   (y-feedback :initform ↑(with1 (mk-elt :span "Y needs more cowbell!")
+                            (set-show-on-feedback it (cell-of ¤y)))))
 
   (:metaclass mvc-class))
-
 
 (set-uri 'text-input-app "/text-input")
 
 
-(defmethod main ((app text-input-app))
-  (with-object app
-    (add-writer-fn (cell-of ¤x) (make-number-parser (cell-of ¤x) t))
-    (add-writer-fn (cell-of ¤y) (make-number-parser (cell-of ¤y) t))))
-
-
 (defmethod render-viewport ((viewport viewport) (app text-input-app))
   (with-object app
-    (remove-all (root))
     (add-to (root)
       (mk-html ()
         (:div
