c36ac0da12b5c7028dbd6caabedd6d98a40dbb14
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-07-17 16:15:13 +0200

    Inline RUN-JS and APPEND-TO-RESPONSE-DATA-OF, and adjust other optimization declarations.

--- src/config-compilation.lisp
+++ src/config-compilation.lisp
@@ -14,9 +14,23 @@
       nil))
 
 
+
+;;; INLINE declarations.
+
+(defmethod maybe-inline ((function-name-designator (eql 'run-js)) &key)
+  t)
+
+
 (defmethod maybe-inline ((function-name-designator (eql 'run)) &key)
   t)
 
+
+(defmethod maybe-inline ((function-name-designator (eql 'append-to-response-data-of)) &key)
+  t)
+
+
+
+;;; OPTIMIZE declarations.
 
 (defmethod optimizations ((context t) &key)
   '(optimize (speed 0) (space 0) (safety 3) (debug 3) (compilation-speed 0)))
@@ -27,24 +41,28 @@
 
 
 (defmethod optimizations ((context (eql :widgets/dom-cache.lisp)) &key)
-  '(optimize (speed 3)))
+  '(optimize (speed 3) (safety 2)))
 
 
 (defmethod optimizations ((context (eql :server-sw-http.lisp)) &key)
-  '(optimize (speed 3)))
+  '(optimize (speed 3) (safety 2)))
 
 
 (defmethod optimizations ((context (eql :ajax.lisp)) &key)
-  '(optimize (speed 3)))
+  '(optimize (speed 3) (safety 2)))
 
 
 (defmethod optimizations ((context (eql :comet.lisp)) &key)
-  '(optimize (speed 3)))
+  '(optimize (speed 3) (safety 2)))
 
 
 (defmethod optimizations ((context (eql :shtml-of)) &key)
-  '(optimize speed (safety 1)))
+  '(optimize (speed 3) (safety 2)))
 
 
 (defmethod optimizations ((context (eql :code.lisp)) &key)
-  '(optimize speed (safety 1)))
+  '(optimize (speed 3) (safety 2)))
+
+
+(defmethod optimizations ((context (eql :viewport.lisp)) &key)
+  '(optimize (speed 3) (safety 2)))
