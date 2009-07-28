3e3452a59093fc4fa9c2d09b4e48cac26a045678
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-07-28 07:39:54 +0200

    ...

--- examples/nostdal.org.lisp
+++ examples/nostdal.org.lisp
@@ -15,13 +15,25 @@
   (let ((main
          (mk-html ()
            (:div
+
             (:h1 "nostdal.org")
             (:em "NOTE: This is being served from an ADSL connection. "
                  "This'll be very slow or even down at random.")
-
             (:ul
              (:li (:a :href "http://gitorious.org/~lnostdal" "gitorious"))
              (:li (:a :href "text-input" "text-input-app")))
+
+            (:h2 "Firefox / Iceweasel extensions")
+            (:p "I keep forgetting the names of these, so as a note-to-self (and maybe others) here is "
+                "some of the must-have stuff:")
+            (:ul
+             (:li "Firebug")
+             (:li "Firecookie")
+             (:li "Flashblock")
+             (:li "Google Gears")
+             (:li "Page Speed (maybe..)")
+             (:li "Tabs Open Relative")
+             (:li "Tiny Menu"))
 
             (:hr :width "100%")
             (:pre "λ(:linux :lighttpd :sbcl :sw-http :sw-mvc :sw-stm :sw-db :symbolicweb)λ")))))
