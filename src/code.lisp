bd23fc262a83d92c3d8d8e62bbd81eefd34496a2
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-07-31 13:13:56 +0200

    RUN and RUN-JS: Don't bother returning anything from these.

--- src/code.lisp
+++ src/code.lisp
@@ -12,14 +12,14 @@ RUN instead."
   (declare (string js-str)
            (viewport viewport))
   (append-to-response-data-of viewport js-str)
-  js-str)
+  (values))
 
 
 #.(maybe-inline 'run)
+(declaim (ftype (function (string (or viewport widget) &key (:server-only-p (member t nil)))
+                          (values &optional))
+                run))
 (defun run (code-str target &key server-only-p)
-  (declare (string code-str)
-           ((or viewport widget) target)
-           ((member t nil) server-only-p))
   "Send JavaScript code to client(s) for execution.
 
 CODE-STR: The JavaScript code.
@@ -28,12 +28,15 @@ TARGET: If supplied with a WIDGET this will transmit the JS code to
 all contexts (browser sessions (users), tabs and windows) where that widget is
 visible. If supplied with a VIEWPORT this will transmit the JS code to that
 single viewport for execution there."
-  (when (string= code-str "")
-    (warn "RUN: (STRING= CODE-STR \"\") => T. Returning from RUN with no effect.")
-    (return-from run))
-  (when server-only-p
-    (return-from run))
-  (if (typep target 'viewport)
-      (run-js code-str target)
-      (when-let (viewport (viewport-of target))
-        (run-js code-str viewport))))
+  (cond
+    (server-only-p)
+
+    ((zerop (length code-str))
+     (warn "SW:RUN: Returning from RUN with no effect; no code in CODE-STR."))
+
+    (t
+     (if (typep target 'viewport)
+         (run-js code-str target)
+         (when-let (viewport (viewport-of target))
+           (run-js code-str viewport)))))
+  (values))
