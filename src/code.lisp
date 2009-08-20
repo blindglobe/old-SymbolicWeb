009bfcdc71c5c4c9800223f7c69661b5fdde919a
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-08-20 17:58:51 +0200

    Remove RUN-JS and call APPEND-TO-RESPONSE-DATA-OF directly instead.

--- src/code.lisp
+++ src/code.lisp
@@ -5,16 +5,6 @@
 (declaim #.(optimizations :code.lisp))
 
 
-#.(maybe-inline 'run-js)
-(defun run-js (js-str viewport)
-  "Send JS-STR (JavaScript code) to VIEWPORT for execution. You should probably use
-RUN instead."
-  (declare (string js-str)
-           (viewport viewport))
-  (append-to-response-data-of viewport js-str)
-  (values))
-
-
 #.(maybe-inline 'run)
 (declaim (ftype (function (string (or viewport widget) &key (:server-only-p (member t nil)))
                           (values &optional))
@@ -28,6 +18,7 @@ TARGET: If supplied with a WIDGET this will transmit the JS code to
 all contexts (browser sessions (users), tabs and windows) where that widget is
 visible. If supplied with a VIEWPORT this will transmit the JS code to that
 single viewport for execution there."
+  (declare (optimize speed))
   (cond
     (server-only-p)
 
@@ -36,7 +27,7 @@ single viewport for execution there."
 
     (t
      (if (typep target 'viewport)
-         (run-js code-str target)
+         (append-to-response-data-of target code-str)
          (when-let (viewport (viewport-of target))
-           (run-js code-str viewport)))))
+           (append-to-response-data-of viewport code-str)))))
   (values))
