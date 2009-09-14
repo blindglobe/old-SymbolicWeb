214ae69fe90acb67f4ca976988605e32ed3c6ef9
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-09-14 17:45:01 +0200

    CONTAINER / CONTAINER-BASE: Don't accept :CHILDREN or :CHILD initargs when doing this new Model -> View thing.

--- src/base-classes.lisp
+++ src/base-classes.lisp
@@ -30,12 +30,3 @@ possible and be able to optimize type-checking code based on this. |#
 Contains View instances. Note that this is also the only hard link to the View part of
 a Model <-> View relationship.")))
 (export '(container-base children children-of))
-
-
-(defmethod initialize-instance :after ((container-base container-base) &key children child)
-  (with-object container-base
-    (setf ¤children
-          (loop :for child :in (mklst (or children child))
-             :collect (if (typep child 'view-base)
-                          child
-                          (view-in-context-of container-base child))))))
