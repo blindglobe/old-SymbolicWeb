c2443b58ba196f6fb5b48a8931ca1bd6d4b4b2ef
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-09-19 17:40:28 +0200

    vecto-3-app; not sure if this looks better ....

--- examples/vecto-3.lisp
+++ examples/vecto-3.lisp
@@ -30,18 +30,18 @@
 
 
 (defmethod render-viewport ((viewport viewport) (app vecto-3-app))
-  (insert (list (with1 (make-instance 'vecto-widget-3)
-                  (setf (position-of it) "absolute"
-                        (width-of it) "50%"
-                        (height-of it) "50%"
-                        (border-of it) "1px solid black"))
-                (with1 (mk-html ()
-                         (:div
-                           (:h3 "Hello World")
-                           (:p "Ok, this is a test of using multiple layers on top of a VECTO widget.")
-                           (:p "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in nisi eget ligula consectetur hendrerit. Nullam pellentesque tincidunt iaculis. Curabitur volutpat aliquam nisl, a aliquet diam consectetur ac. Suspendisse sit amet magna a lacus bibendum rhoncus sit amet id tortor. Ut risus sem, pharetra eget mattis eget, posuere ac risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer at eros id elit condimentum vestibulum at id leo. Cras a tellus eu nunc condimentum venenatis ac id lectus. Nam non tellus tellus. Quisque eget vestibulum elit. ")))
-                  (setf (overflow-of it) "auto"
-                        (position-of it) "absolute"
-                        (width-of it) "50%"
-                        (height-of it) "50%")))
-          :in (root)))
+  (let ((vecto (make-instance 'vecto-widget-3)))
+    (setf (position-of vecto) "absolute"
+          (width-of vecto) "50%"
+          (height-of vecto) "50%"
+          (border-of vecto) "1px solid black")
+    (with (mk-html ()
+            (:div
+              (:h3 "Hello World")
+              (:p "Ok, this is a test of using multiple layers on top of a VECTO widget.")
+              (:p "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in nisi eget ligula consectetur hendrerit. Nullam pellentesque tincidunt iaculis. Curabitur volutpat aliquam nisl, a aliquet diam consectetur ac. Suspendisse sit amet magna a lacus bibendum rhoncus sit amet id tortor. Ut risus sem, pharetra eget mattis eget, posuere ac risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer at eros id elit condimentum vestibulum at id leo. Cras a tellus eu nunc condimentum venenatis ac id lectus. Nam non tellus tellus. Quisque eget vestibulum elit. ")))
+      (setf (position-of it) "absolute"
+            (height-of it) "100%"
+            (overflow-of it) "auto")
+      (insert it :in vecto))
+    (insert vecto :in (root))))
