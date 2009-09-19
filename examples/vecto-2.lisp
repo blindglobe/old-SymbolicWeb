4eeb584c2ee4460d8b0b8f5472780fbefdbe0054
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-09-19 14:03:53 +0200

    ..updates to vecto-2 example..
    make it a bit clear that this is actually a widget now.

--- examples/vecto-2.lisp
+++ examples/vecto-2.lisp
@@ -1,31 +1,36 @@
 (in-package #:sw)
 
 
+(defclass my-vecto-widget (vecto)
+  ((vc-font :reader vc-font-of
+            :initform (zpb-ttf:open-font-loader "/usr/share/fonts/truetype/msttcorefonts/times.ttf")))
+
+  (:default-initargs
+   :filename "vecto-2-app"))
+
+
+(defmethod redraw ((vc my-vecto-widget))
+  (vecto:set-rgb-stroke 0 1 0)
+  (vecto:set-line-width 10)
+  (vecto:rectangle 0 0 (inner-width-of vc) (inner-height-of vc))
+  (vecto:stroke)
+  (vecto-simple-draw-string "Hello Lisp World!"
+                            (vc-font-of vc)
+                            (inner-width-of vc)
+                            (inner-height-of vc)))
+
+
+
 (defclass vecto-2-app (application)
-  ((font :allocation :class
-         :initform (zpb-ttf:open-font-loader "/usr/share/fonts/truetype/msttcorefonts/times.ttf"))))
+  ())
 
 (set-uri 'vecto-2-app "/vecto-2")
 
 
 (defmethod render-viewport ((viewport viewport) (app vecto-2-app))
-  (insert (with1 (make-instance 'vecto)
-                  ;; CSS stuff.
+  (insert (with1 (make-instance 'my-vecto-widget)
             (setf (position-of it) "absolute"
                   (width-of it) "50%"
                   (height-of it) "50%"
-                  (border-of it) "1px solid black"
-
-                  ;; VECTO stuff.
-                  (filename-of it) "vecto-2-app"
-                  (redraw-fn-of it)
-                  (lambda (vc)
-                    (vecto:set-rgb-stroke 0 1 0)
-                    (vecto:set-line-width 10)
-                    (vecto:rectangle 0 0 (inner-width-of vc) (inner-height-of vc))
-                    (vecto:stroke)
-                    (vecto-simple-draw-string "Hello Lisp World!"
-                                              (slot-value app 'font)
-                                              (inner-width-of vc)
-                                              (inner-height-of vc)))))
+                  (border-of it) "1px solid black"))
           :in (root)))
