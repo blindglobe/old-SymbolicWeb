7824b057ca6a65844e2ae5eed332f65d4ce1ce14
Lars Rune Nøstdal <larsnostdal@gmail.com>
2009-10-02 21:59:08 +0200

    FOCUSSABLE: Works again.
    If this turns out to be wrong; document where the "CELL formula" is or should be stored.

--- src/widgets/focussable.lisp
+++ src/widgets/focussable.lisp
@@ -6,11 +6,25 @@
 
 
 #| TODO:
-Add optional focus tracking using a custom ON-FOCUS event.
+Add optional focus tracking using a custom ON-FOCUS event. (wait .. why or, what?)
+
+To do this stuff proper (and move the LAST-FOCUS slot from the APPLICATION class to the VIEWPORT class) we must not
+create a new VIEWPORT every time the user refreshes his page. To do this, I think (ok, I'm pretty sure; cookies won't
+work for instance) the client needs to store the 'viewport-id' in the URL hash instead of storing it as a global
+JS variable as done now.
 |#
+
 
 ;; The machinery for this thing is in (RENDER-VIEWPORT :AFTER (VIEWPORT APPLICATION)) in viewport.lisp.
 (defclass focussable ()
   ()
   (:default-initargs
    :focussable-p t))
+
+
+(defmethod initialize-instance :after ((focussable focussable) &key)
+  (with-formula focussable
+    (when (and (on-focus-of focussable)
+               (focussable-p-of focussable))
+      (setf (slot-value *app* 'last-focus)
+            focussable))))
