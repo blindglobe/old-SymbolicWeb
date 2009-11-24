3ccabe990ce2d848eca2366c5cd0ba3e4e20a227
Lars Rune Nøstdal <lnostdal@blackbox.(none)>
2009-11-24 01:31:06 +0100

    dom-cache: Isn't EVAL "good enough" here?

--- src/widgets/dom-cache.lisp
+++ src/widgets/dom-cache.lisp
@@ -92,63 +92,63 @@ Strong hash table; ID->CALLBACK-BOX.")))
   (setf (get lisp-accessor-name 'value-marshaller) value-marshaller)
 
   ;; Add DOM reader.
-  (compile-and-execute
-    `(defun ,lisp-reader-name (dom-mirror)
-       (declare #.(optimizations :dom-property)
-                (dom-mirror dom-mirror))
-       (if *js-code-only-p*
-           (funcall ,dom-client-reader dom-mirror)
-           (multiple-value-bind (value found-p)
-               (funcall ,dom-server-reader dom-mirror ',lisp-accessor-name)
-             (if found-p
-                 (values value found-p)
-                 ,(when dom-server-reader-fallback-value-supplied-p
-                   `(values ,dom-server-reader-fallback-value :fallback)))))))
+  (eval
+   `(defun ,lisp-reader-name (dom-mirror)
+      (declare #.(optimizations :dom-property)
+               (dom-mirror dom-mirror))
+      (if *js-code-only-p*
+          (funcall ,dom-client-reader dom-mirror)
+          (multiple-value-bind (value found-p)
+              (funcall ,dom-server-reader dom-mirror ',lisp-accessor-name)
+            (if found-p
+                (values value found-p)
+                ,(when dom-server-reader-fallback-value-supplied-p
+                       `(values ,dom-server-reader-fallback-value :fallback)))))))
 
   ;; Add DOM writer.
-  (compile-and-execute
-    `(defun ,lisp-writer-name (property-value dom-mirror &rest args &key client-only-p &allow-other-keys)
-       (declare #.(optimizations :dom-property)
-                (dom-mirror dom-mirror)
-                ((member nil t) client-only-p))
-       (delete-from-plistf args :client-only-p)
-       (let ((remove-entry-p
-              (and ,value-removal-checker (funcall ,value-removal-checker property-value))))
-         (flet ((client-writer ()
-                  (if remove-entry-p
-                      ;; Remove entry on client side.
-                      (apply ,dom-client-remover dom-mirror args)
-                      ;; Add and/or set entry on client side.
-                      (apply ,dom-client-writer
-                             ,(if value-marshaller
-                                  `(funcall ,value-marshaller property-value)
-                                  `property-value)
-                             dom-mirror args)))
-                (server-writer ()
-                  (unless client-only-p
-                    (if remove-entry-p
-                        ;; Remove entry on server side.
-                        (funcall ,dom-server-remover dom-mirror ',lisp-accessor-name)
-                        ;; Add and/or set entry on server side.
-                        (funcall ,dom-server-writer dom-mirror ',lisp-accessor-name property-value)))))
-           (declare (inline client-writer server-writer))
-           (if *js-code-only-p*
-               (client-writer)
-               (prog1 property-value
-                 (server-writer)
-                 (client-writer)))))))
+  (eval
+   `(defun ,lisp-writer-name (property-value dom-mirror &rest args &key client-only-p &allow-other-keys)
+      (declare #.(optimizations :dom-property)
+               (dom-mirror dom-mirror)
+               ((member nil t) client-only-p))
+      (delete-from-plistf args :client-only-p)
+      (let ((remove-entry-p
+             (and ,value-removal-checker (funcall ,value-removal-checker property-value))))
+        (flet ((client-writer ()
+                 (if remove-entry-p
+                     ;; Remove entry on client side.
+                     (apply ,dom-client-remover dom-mirror args)
+                     ;; Add and/or set entry on client side.
+                     (apply ,dom-client-writer
+                            ,(if value-marshaller
+                                 `(funcall ,value-marshaller property-value)
+                                 `property-value)
+                            dom-mirror args)))
+               (server-writer ()
+                 (unless client-only-p
+                   (if remove-entry-p
+                       ;; Remove entry on server side.
+                       (funcall ,dom-server-remover dom-mirror ',lisp-accessor-name)
+                       ;; Add and/or set entry on server side.
+                       (funcall ,dom-server-writer dom-mirror ',lisp-accessor-name property-value)))))
+          (declare (inline client-writer server-writer))
+          (if *js-code-only-p*
+              (client-writer)
+              (prog1 property-value
+                (server-writer)
+                (client-writer)))))))
 
   ;; Add DOM renderer.
-  (compile-and-execute
-    ;; TOOD: Using a method to dispatch wrt. PROPERTY-NAME might be a bit overkill; try to think of a faster way?
-    `(defmethod render-dom ((dom-mirror dom-mirror) (property-name (eql ',lisp-accessor-name)) property-value)
-       (declare #.(optimizations :dom-property))
-       (run (js-code-of (funcall ,dom-client-writer
-                                 ,(if value-marshaller
-                                      `(funcall ,value-marshaller property-value)
-                                      `property-value)
-                                 dom-mirror))
-            dom-mirror))))
+  (eval
+   ;; TOOD: Using a method to dispatch wrt. PROPERTY-NAME might be a bit overkill; try to think of a faster way?
+   `(defmethod render-dom ((dom-mirror dom-mirror) (property-name (eql ',lisp-accessor-name)) property-value)
+      (declare #.(optimizations :dom-property))
+      (run (js-code-of (funcall ,dom-client-writer
+                                ,(if value-marshaller
+                                     `(funcall ,value-marshaller property-value)
+                                     `property-value)
+                                dom-mirror))
+           dom-mirror))))
 (export 'define-dom-property)
 
 
