aa56da21cb3eca7fec9a705bf6529bcad5f8785e
Lars Rune Nøstdal <lnostdal@blackbox.(none)>
2009-12-09 20:00:19 +0100

    ..cleanups..

--- src/empty-page.lisp
+++ src/empty-page.lisp
@@ -11,15 +11,11 @@
 
 
 
-#|(defmethod render-viewport ((viewport viewport) (app empty-page-app))
-  ;;(dotimes (i 10)
-  (add (make-instance 'html-element :model square-of-x) (root)))|#
-
+(defmethod render-viewport ((viewport viewport) (app empty-page-app))
+  (insert λV"Hello World!" :in (root)))
 
 
 (defmethod generate-dynamic-subdomain ((app empty-page-app))
-  ;; To break the 2 connection limit of HTTP do something like
+  ;; To break the 2 connection limit of HTTP do something like:
   ;;   (catstr "sw.dyn-" (generate-id))
-  ;; ..here (in your application subclass).
   nil)
-  ;;(catstr "sw.dyn-" (generate-id)))
